abe72fdfed2049373ee4979074baeb07c66b5554
Hassan <hassan@gmail.com>
2026-08-22 19:45:03 +0600

    get the destination

--- src/app/destinations/page.jsx
+++ src/app/destinations/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import DestinationCard from "@/components/DestinationCard";
 
-const DestinationPage = () => {
+const DestinationPage = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/destination`)
+    const destinations = await res.json() 
+
+
     return (
-        <div>
-            this is destination
+        <div className="max-w-7xl mx-auto">
+            <h1>All destinations</h1>
+
+
+            <div className="grid grid-cols-4 gap-5">
+                {
+                    destinations.map(destination => <DestinationCard key={destination._id} destination={destination}/>)
+                }
+
+            </div>
+
         </div>
     );
 };

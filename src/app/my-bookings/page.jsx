f59abad57a7759856dd95684c364673d13914993
Hassan <hassan@gmail.com>
2026-08-22 19:32:31 +0600

    post done

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -1,9 +1,18 @@
+import { b } from 'framer-motion/client';
 import React from 'react';
 
-const MyBookingsPage = () => {
+const MyBookingsPage = async() => {
+ const res = await fetch('http://localhost:5000/users')
+ const bookings = await res.json()
+
     return (
         <div>
+
             this is bookings
+            {
+                bookings.map(b=><div key={b.id}>{
+            b.name}</div>)
+            }
         </div>
     );
 };
